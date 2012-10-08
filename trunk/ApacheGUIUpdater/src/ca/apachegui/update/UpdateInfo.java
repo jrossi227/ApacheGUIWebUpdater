a78cbb9a5084c1c3152a0c272bb29f637b8c8659
jrossi227 <jrossi227@2e909810-a851-461d-a380-397dba9799fe>
2012-10-08 04:46:31 +0000

    Initial Updater Commit

---
+++ trunk/ApacheGUIUpdater/src/ca/apachegui/update/UpdateInfo.java
@@ -0,0 +1,47 @@
+package ca.apachegui.update;
+
+public class UpdateInfo {
+	private String version;
+	private String size;
+	private String details;
+	private String url;
+
+	public UpdateInfo(String version, String size, String details, String url) {
+		this.version=version;
+		this.size=size;
+		this.details=details;
+		this.setUrl(url);
+	}
+	
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
